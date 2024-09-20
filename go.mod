abe9ab47e8b5f2939e67440b091201263df2c8bc
hellisham <ashkan.m.m.t@gmail.com>
2024-09-20 13:45:46 +0330

    second

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Hellisham/course
+
+go 1.23.0
---
+++ puppy.go
@@ -0,0 +1,11 @@
+package puppy
+
+import "fmt"
+
+func Bark() string {
+	fmt.Println("woof")
+}
+
+func barks() string {
+	fmt.Println("woof! woof! woof!")
+}
